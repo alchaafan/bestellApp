6949efbc599616e6fccf6c380a58f31d66467c78
Muhammad <m.shaafan@outlook.de>
2025-04-23 12:45:59 +0200

    update

--- scripts/templates.js
+++ scripts/templates.js
@@ -22,10 +22,15 @@ function getFoodTemplates(i) {
 function addToCartTemplates(i) {
     return `<div id="cartList">
         <li>${myFood[i].name} ${myFood[i].price} € - 
-        <span id="count${i}">1</span>
+        
         </li>
+        <div id="buttonsContainer">
+        <div id="plusminusContainer">
         <img onclick="increaseCount(${i})" class="plusminus" src="./img/plus.png" alt="">
+        <span id="count${i}">1</span>
         <img  onclick="decreaseCount(${i})" class="plusminus" src="./img/minus.png" alt="">
+        </div>
         <img " id="delete" src="./img/delete.png" alt="">
+        </div>
     </div>`;
 }
